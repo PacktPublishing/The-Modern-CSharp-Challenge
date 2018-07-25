f962bd710b893fa404fd9782ec2dd8537bdb3344
WriterRod <41203701+WriterRod@users.noreply.github.com>
2018-07-25 16:52:09 -0600

    Add files via upload

---
+++ Chapter01/AmicableNumbers/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace AmicableNumbers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numPairsLabel = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.resultListBox = new System.Windows.Forms.ListBox();
+            this.goButton = new System.Windows.Forms.Button();
+            this.maxTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // numPairsLabel
+            // 
+            this.numPairsLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.numPairsLabel.AutoSize = true;
+            this.numPairsLabel.Location = new System.Drawing.Point(61, 231);
+            this.numPairsLabel.Name = "numPairsLabel";
+            this.numPairsLabel.Size = new System.Drawing.Size(0, 17);
+            this.numPairsLabel.TabIndex = 28;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 231);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 17);
+            this.label4.TabIndex = 27;
+            this.label4.Text = "Pairs:";
+            // 
+            // resultListBox
+            // 
+            this.resultListBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.resultListBox.FormattingEnabled = true;
+            this.resultListBox.IntegralHeight = false;
+            this.resultListBox.ItemHeight = 16;
+            this.resultListBox.Location = new System.Drawing.Point(12, 41);
+            this.resultListBox.Name = "resultListBox";
+            this.resultListBox.Size = new System.Drawing.Size(358, 187);
+            this.resultListBox.TabIndex = 2;
+            // 
+            // goButton
+            // 
+            this.goButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.goButton.Location = new System.Drawing.Point(295, 12);
+            this.goButton.Name = "goButton";
+            this.goButton.Size = new System.Drawing.Size(75, 23);
+            this.goButton.TabIndex = 1;
+            this.goButton.Text = "Go";
+            this.goButton.UseVisualStyleBackColor = true;
+            this.goButton.Click += new System.EventHandler(this.goButton_Click);
+            // 
+            // maxTextBox
+            // 
+            this.maxTextBox.Location = new System.Drawing.Point(54, 12);
+            this.maxTextBox.Name = "maxTextBox";
+            this.maxTextBox.Size = new System.Drawing.Size(100, 22);
+            this.maxTextBox.TabIndex = 0;
+            this.maxTextBox.Text = "100000";
+            this.maxTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 17);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "Max:";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.goButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 253);
+            this.Controls.Add(this.numPairsLabel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.resultListBox);
+            this.Controls.Add(this.goButton);
+            this.Controls.Add(this.maxTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "AmicableNumbers";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label numPairsLabel;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox resultListBox;
+        private System.Windows.Forms.Button goButton;
+        private System.Windows.Forms.TextBox maxTextBox;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
--- Chapter01/AmicableNumbers/Form1.cs
+++ Chapter01/AmicableNumbers/Form1.cs
@@ -1,1 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
+namespace AmicableNumbers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Display amicable numbers.
+        private void goButton_Click(object sender, EventArgs e)
+        {
+            resultListBox.Items.Clear();
+            numPairsLabel.Text = "";
+            Cursor = Cursors.WaitCursor;
+            Refresh();
+
+            long max = long.Parse(maxTextBox.Text);
+            List<List<long>> pairs = FindAmicablePairs(max);
+            foreach (List<long> pair in pairs)
+                resultListBox.Items.Add(pair[0] + ", " + pair[1]);
+
+            numPairsLabel.Text = resultListBox.Items.Count.ToString();
+            Cursor = Cursors.Default;
+        }
+
+        // Find amicable numbers <= max.
+        private List<List<long>> FindAmicablePairs(long max)
+        {
+            // Get the array of sums.
+            long[] sums = GetSumsOfDivisors(max);
+
+            // Look for pairs.
+            List<List<long>> pairs = new List<List<long>>();
+            for (int value = 1; value <= max; value++)
+            {
+                long sum = sums[value];
+                if ((sum > value) && (sum <= max) && (sums[sum] == value))
+                {
+                    List<long> pair = new List<long>();
+                    pair.Add(value);
+                    pair.Add(sums[value]);
+                    pairs.Add(pair);
+                }
+            }
+
+            return pairs;
+        }
+
+        // Calculate the sums of the divisors of numbers between 1 and max.
+        private long[] GetSumsOfDivisors(long max)
+        {
+            // Make room for the sums.
+            long[] sums = new long[max + 1];
+
+            // Fill in the sums.
+            for (long i = 1; i <= max; i++)
+                sums[i] = GetProperDivisors(i).Sum();
+
+            // Return the result.
+            return sums;
+        }
+
+        // Return this number's proper divisors.
+        private List<long> GetProperDivisors(long number)
+        {
+            checked
+            {
+                List<long> divisors = new List<long>();
+                divisors.Add(1);
+
+                long limit = (long)Math.Sqrt(number);
+                for (long divisor = 2; divisor <= limit; divisor++)
+                {
+                    if (number % divisor == 0)
+                    {
+                        divisors.Add(divisor);
+
+                        long divisor2 = number / divisor;
+                        if (divisor != divisor2) divisors.Add(divisor2);
+                    }
+                }
+                divisors.Sort();
+                return divisors;
+            }
+        }
+    }
+}
