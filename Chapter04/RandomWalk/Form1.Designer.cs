38ced75136a66e3881df46899066234becf52d68
WriterRod <41203701+WriterRod@users.noreply.github.com>
2018-08-29 09:48:17 -0600

    Add files via upload

---
+++ Chapter04/RandomWalk/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace RandomWalk
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.walkPictureBox = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numStepsTextBox = new System.Windows.Forms.TextBox();
+            this.stepSizeTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.walkButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.walkPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // walkPictureBox
+            // 
+            this.walkPictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.walkPictureBox.BackColor = System.Drawing.Color.White;
+            this.walkPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.walkPictureBox.Location = new System.Drawing.Point(12, 67);
+            this.walkPictureBox.Name = "walkPictureBox";
+            this.walkPictureBox.Size = new System.Drawing.Size(300, 300);
+            this.walkPictureBox.TabIndex = 0;
+            this.walkPictureBox.TabStop = false;
+            this.walkPictureBox.Paint += new System.Windows.Forms.PaintEventHandler(this.walkPictureBox_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "# Steps:";
+            // 
+            // numStepsTextBox
+            // 
+            this.numStepsTextBox.Location = new System.Drawing.Point(65, 12);
+            this.numStepsTextBox.Name = "numStepsTextBox";
+            this.numStepsTextBox.Size = new System.Drawing.Size(61, 20);
+            this.numStepsTextBox.TabIndex = 2;
+            this.numStepsTextBox.Text = "1000";
+            this.numStepsTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // stepSizeTextBox
+            // 
+            this.stepSizeTextBox.Location = new System.Drawing.Point(211, 12);
+            this.stepSizeTextBox.Name = "stepSizeTextBox";
+            this.stepSizeTextBox.Size = new System.Drawing.Size(61, 20);
+            this.stepSizeTextBox.TabIndex = 4;
+            this.stepSizeTextBox.Text = "5";
+            this.stepSizeTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(150, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Step Size:";
+            // 
+            // walkButton
+            // 
+            this.walkButton.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.walkButton.Location = new System.Drawing.Point(125, 38);
+            this.walkButton.Name = "walkButton";
+            this.walkButton.Size = new System.Drawing.Size(75, 23);
+            this.walkButton.TabIndex = 5;
+            this.walkButton.Text = "Walk";
+            this.walkButton.UseVisualStyleBackColor = true;
+            this.walkButton.Click += new System.EventHandler(this.walkButton_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.walkButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(324, 379);
+            this.Controls.Add(this.walkButton);
+            this.Controls.Add(this.stepSizeTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numStepsTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.walkPictureBox);
+            this.Name = "Form1";
+            this.Text = "RandomWalk";
+            ((System.ComponentModel.ISupportInitialize)(this.walkPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox walkPictureBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox numStepsTextBox;
+        private System.Windows.Forms.TextBox stepSizeTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button walkButton;
+    }
+}
+
---
+++ Chapter04/RandomWalk/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RandomWalk
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random Rand = new Random();
+        Point[] Points = null;
+
+        // Generate a random walk.
+        private void walkButton_Click(object sender, EventArgs e)
+        {
+            // Get parameters.
+            int numSteps = int.Parse(numStepsTextBox.Text);
+            int stepSize = int.Parse(stepSizeTextBox.Text);
+
+            // Start in the center of the PictureBox.
+            int x = walkPictureBox.ClientSize.Width / 2;
+            int y = walkPictureBox.ClientSize.Height / 2;
+
+            // Build the points.
+            Points = new Point[numSteps];
+            for (int i = 0; i < numSteps; i++)
+            {
+                Points[i] = new Point(x, y);
+                switch (Rand.Next(0, 4))
+                {
+                    case 0:             // Up.
+                        y -= stepSize;
+                        break;
+                    case 1:             // Right.
+                        x += stepSize;
+                        break;
+                    case 2:             // Down.
+                        y += stepSize;
+                        break;
+                    case 3:             // Left.
+                        x -= stepSize;
+                        break;
+                }
+            }
+
+            // Redraw.
+            walkPictureBox.Refresh();
+        }
+
+        // Draw the walk.
+        private void walkPictureBox_Paint(object sender, PaintEventArgs e)
+        {
+            if (Points == null) return;
+
+            e.Graphics.Clear(walkPictureBox.BackColor);
+            e.Graphics.DrawLines(Pens.Blue, Points);
+        }
+    }
+}
